6342b0ba844fd5ae1d1a836f97c1581651a13f73
minkyu.kim2 <minkyu.kim2@IC-CS233-11.uc.ucalgary.ca>
2022-07-23 19:29:21 -0600

    created classes ShoppingCartView and Item

---
+++ src/application/ShoppingCartViewer.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.util.ArrayList;
+
+// receives list of items and displays the items and the total price 
+public class ShoppingCartViewer {
+	private ArrayList<Item> items;
+	private double totalCost; 
+	
+	public ShoppingCartViewer(ArrayList<Item> items) {
+		
+	}
+	
+}
